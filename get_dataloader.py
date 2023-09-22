59a3bdca613d00a876ee55ec15dbec798abe0a16
sbc806 <sarahc19233@gmail.com>
2023-09-22 18:04:32 +0000

    Changed the way items are drawn.

--- get_dataloader.py
+++ get_dataloader.py
@@ -127,14 +127,15 @@ class MnistptsDataset(data.Dataset):
             # pass
             weights = torch.ones(pts.shape[0])
             self.sample_indices = torch.multinomial(weights, num_pts)
-            print(sample_indices)
-            pts_sampled = pts[sample_indices]
+            print(self.sample_indices)
+            pts_sampled = pts[self.sample_indices]
             # random_ordering = torch.randperm(len(self.pts_list))
             # pts_sampled = self.pts_list[random_ordering[0: num_pts]]
         else:
             # pass
             self.sample_indices = torch.arange(num_pts)
-            pts_sampled = pts[0: num_pts]
+            print(self.sample_indices)
+            pts_sampled = pts[self.sample_indices]
 
         return pts_sampled
 
@@ -144,7 +145,7 @@ class MnistptsDataset(data.Dataset):
         # TODO: get item from dataset.
         # Note that we expect: pc (np.float32 type), label(np.int)
         individual_pc = self.pts_list[index]
-        individual_pc_label = self.labels[index]
+        individual_pc_labels = self.labels[index]
         data = {"pc": self.random_sampling(individual_pc, self.num_pts),
                 "label": individual_pc_labels[self.sample_indices]}
         print(self.sample_indices)
